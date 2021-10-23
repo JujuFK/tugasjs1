8efb29702a2082a6d6554c0d97a9cc44d854124c
JujuFK <juan.frederick58@gmail.com>
2021-10-23 14:02:02 +0700

    first commit

---
+++ program.js
@@ -0,0 +1,8 @@
+var nama = prompt("Masukan nama anda");
+var confirm = confirm("Hallo " + nama + " Apakah saya tampan?");
+
+if(confirm == true){
+    alert("Terima Kasih!!");
+} else{
+    alert("Whyy??!!");
+}
